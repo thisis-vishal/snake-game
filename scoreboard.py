7b45088ef325100a46df2f7dc5071c7a54f9bceb
Vishal Kumar <76644828+thisis-vishal@users.noreply.github.com>
2021-06-19 11:28:14 +0530

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,19 +1,31 @@
-from turtle import Turtle
+from turtle import Turtle, write
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score=0
+        with open("highscore.txt") as file:
+            score=int(file.read())
+            self.highscore=score
         self.penup()
         self.color("white")
         self.hideturtle()
         self.goto(0,270)
-        self.write(f"score:{self.score}",move=False,align="center",font=("Arial",24,"normal"))
+        self.write(f"score:{self.score}  High score: {self.highscore}",move=False,align="center",font=("Arial",24,"normal"))
     def incscore(self):
         self.score+=1 
+        self.updatescoreboard()
+    def updatescoreboard(self):    
         self.clear()  
-        self.write(f"score:{self.score}",move=False,align="center",font=("Arial",24,"normal"))
-    def gameover(self):
-        self.goto(0,0)
-        self.write(f"GAME OVER.",align="center",font=("Arial",24,"normal"))
+        self.write(f"score:{self.score}   High score: {self.highscore}",move=False,align="center",font=("Arial",24,"normal"))
+    def reset(self):
+        if self.score>self.highscore:
+            with open("highscore.txt",mode="w") as file:
+                file.write(f"{self.score}")
+                self.highscore=self.score
+        self.score=0    
+        self.updatescoreboard()     
+    # def gameover(self):
+    #     self.goto(0,0)
+    #     self.write(f"GAME OVER.",align="center",font=("Arial",24,"normal"))
 
          
